6e26ed5d854865f48cdd86969183c2663e71e0b3
May Hoang <mayhoang@MBP-de-May.cust.communityfibre.co.uk>
2024-01-28 13:17:01 +0000

    style project cards:

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -8,21 +8,22 @@ const Projects = () => {
 	return (
 		<section id="projects">
 			<h2 className="my-2">Projects</h2>
-			<div className="flex flex-wrap flex-row gap-6">
+			<div className="flex flex-wrap flex-row gap-8">
 				{
 					projects.map(project => (
-						<div key={project.title} className="max-w-80 p-3 border-solid border-2 border-blue-300 ">
+						<div key={project.title} className="max-w-80 p-3 bg-slate-50">
 							<h3>{project.title}</h3>
 							<Image src={project.imageSrc} width={500}
 								height={500} alt={`Image of ${project.imageSrc}`} className="my-2"/>
-							<Link href={project.demo} target="_blank" className="border-solid border-2 border-blue-300 p-1 text-base">View<MdOutlinePageview size="26px" className="inline ml-1 my-2"/></Link>
-							<Link href={project.source}  target="_blank" className="border-solid border-2 border-blue-300 p-1 text-base ml-2">Github<FaGithub size="18px" className="inline ml-1"/></Link>
-							<p>{project.description}</p>
+
+							<p className="py-1">{project.description}</p>
 							<ul>
 								{project.skills.map(skill => (
-									<li className="border-solid border-2 border-sky-950 w-fit inline-block mr-1 mt-1 px-1 text-xs" key={skill}>{skill}</li>
+									<li className="font-medium bg-blue-300 w-fit rounded-md inline-block mr-2 mt-1 px-1 text-xs" key={skill}>{skill}</li>
 								))}
 							</ul>
+							<Link href={project.demo} target="_blank" className="border-solid border-2 border-blue-300  p-1 text-base rounded-md bg-blue-100 hover:bg-blue-300 font-medium">View<MdOutlinePageview size="26px" className="inline ml-1 my-2"/></Link>
+							<Link href={project.source}  target="_blank" className="border-solid border-2 border-blue-300 p-1 text-base ml-2 rounded-md bg-blue-100  hover:bg-blue-300 font-medium">Github<FaGithub size="18px" className="inline ml-1"/></Link>
 						</div>
 					))
 				}
