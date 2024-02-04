e3f4f5e66b6795d7c5edd29460a28bbc5b771c27
May Hoang <mayhoang@MBP-de-May.cust.communityfibre.co.uk>
2024-02-04 11:55:53 +0000

    skills icom styling

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -4,12 +4,12 @@ const Skills = () => {
 
 	return (
 		<section id="skills" className="px-10">
-			<h2>Skills</h2>
-			{icons.map(icon => (
-				<img src={icon.src} key={icon.skill} width={80}/>
-			))}
-			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, fugiat ex necessitatibus unde ab omnis modi dolor rerum quam. Aliquid hic dolorem quia maxime neque? Vel libero nostrum veniam harum.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus voluptas repellat deleniti sequi. Nulla ipsa commodi, nesciunt quisquam, at cupiditate animi delectus sed, quibusdam iure ratione reiciendis laborum quas fuga.</p>
+			<h2 className="pt-6 pb-1">Skills</h2>
+			<div className="flex flex-row flex-wrap justify-between">
+				{icons.map(icon => (
+					<img src={icon.src} key={icon.skill} width={80} className="my-2"/>
+				))}
+			</div>
 		</section>
 	)
 }
