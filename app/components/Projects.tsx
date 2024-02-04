b9c163a555e5ec4ac5731df737edbfa93bc505ec
May Hoang <mayhoang@MBP-de-May.cust.communityfibre.co.uk>
2024-02-04 11:55:19 +0000

    update card styling

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -9,17 +9,16 @@ import { MdOutlinePageview } from "react-icons/md";
 const Projects = () => {
 	return (
 		<section id="projects" className="px-10">
-			<h2 className="my-2">Projects</h2>
+			<h2 className="my-2 pt-6 pb-1">Projects</h2>
 			<div className="flex flex-wrap flex-row justify-between">
 				{
 					projects.map(project => (
-						<div key={project.title} className="md:grow md:max-w-[48%] w-full p-3 bg-slate-50 mb-10">
-							
+						<div key={project.title} className="md:grow md:max-w-[48%] w-full p-4 bg-slate-50 mb-10 rounded-md">
+							<h3 className="pb-2">{project.title}</h3>
 							<Image src={project.imageSrc} width={600}
-								height={500} alt={`Image of ${project.imageSrc}`} className="mb-2"/>
+								height={500} alt={`Image of ${project.imageSrc}`} className="mb-2 rounded-md border-solid border-2 border-slate-600"/>
 							<Link href={project.demo} target="_blank" className="border-solid border-2 border-blue-300  p-1 text-base rounded-md bg-blue-100 hover:bg-blue-300 font-medium">View<MdOutlinePageview size="26px" className="inline ml-1 my-2"/></Link>
 							<Link href={project.source}  target="_blank" className="border-solid border-2 border-blue-300 p-1 text-base ml-2 rounded-md bg-blue-100  hover:bg-blue-300 font-medium">GitHub<FaGithub size="18px" className="inline ml-1"/></Link>
-							<h3>{project.title}</h3>
 							<p className="py-1">{project.description}</p>
 							<ul>
 								{project.skills.map(skill => (
