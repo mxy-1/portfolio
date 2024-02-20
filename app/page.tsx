234f837a5b7184e120c0822c8912945201664822
May Hoang <mayhoang@MBP-de-May.cust.communityfibre.co.uk>
2024-02-20 09:48:41 +0000

    remove comments

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
-//  className="px-10 py-6" add to divs instead
 export default function Home() {
     return (
         <div >
